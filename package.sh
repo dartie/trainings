81f00b1e3caef6195fb3399389b57cfb4c2c2938
Dario Necco <dartie90@gmail.com>
2022-04-23 10:58:37 +0200

    run package only if html is produced correctly

--- package.sh
+++ package.sh
@@ -1,2 +1,5 @@
 python3 create-preso.py $1.md -t nalug -b $1/nalug.png
-tar -zcvf $1.tar.gz dist plugin common $1 $1.html
+
+if [ $? -eq 0 ]; then
+    tar -zcvf $1.tar.gz dist plugin common $1 $1.html
+fi
